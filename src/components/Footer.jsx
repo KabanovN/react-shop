f4dfb7fec857e10c17cbb22fa799907610570691
Nikita <kabanov.n.v@mail.ru>
2022-03-23 20:11:25 +0300

    окончание проекта

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,11 @@ function Footer() {
             <div className='footer-copyright'>
                 <div className='container'>
                     © {new Date().getFullYear()} Copyright Text
-                    <a className='grey-text text-lighten-4 right' href='#!'>
+                    <a
+                        className='grey-text text-lighten-4 right'
+                        href='https://kabanovn.github.io/react-shop/'
+                        target='_blank'
+                        rel='noreferrer'>
                         Repo
                     </a>
                 </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,17 @@ function Header() {
     return (
         <nav className='green darken-3'>
             <div className='nav-wrapper'>
-                <a href='#!' className='brand-logo'>
+                <a href='/' className='brand-logo'>
                     React Shop
                 </a>
                 <ul id='nav-mobile' className='right hide-on-med-and-down'>
                     <li>
-                        <a href='#!'>Repo</a>
+                        <a
+                            href='https://kabanovn.github.io/react-shop/'
+                            target='_blank'
+                            rel='noreferrer'>
+                            Repo
+                        </a>
                     </li>
                 </ul>
             </div>
